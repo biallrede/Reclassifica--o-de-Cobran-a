ef795460dd2357b8671adbb7a4e272d46968aee9
biallrede <biallrede@gmail.com>
2024-09-04 17:47:21 -0300

    subindo conexão correta no banco

--- credentials.py
+++ credentials.py
@@ -24,25 +24,25 @@ def credenciais_banco_teste():
 
 
 
-# def credenciais_banco_token():
-# # Configuração da conexão com o banco de dados
-#     conn = pyodbc.connect(
-#         'DRIVER={ODBC Driver 18 for SQL Server};'
-#         'Server=187.121.151.19;' 
-#         'Database=DB_BASE;'
-#         'UID=user_allnexus;'
-#         'PWD=uKl041xn8HIw0WF;'
-#         'TrustServerCertificate=yes;'
-#     )
-#     return conn
-
 def credenciais_banco_token():
 # Configuração da conexão com o banco de dados
     conn = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'DRIVER={ODBC Driver 18 for SQL Server};'
         'Server=187.121.151.19;' 
         'Database=DB_BASE;'
         'UID=user_allnexus;'
         'PWD=uKl041xn8HIw0WF;'
+        'TrustServerCertificate=yes;'
     )
     return conn
+
+# def credenciais_banco_token():
+# # Configuração da conexão com o banco de dados
+#     conn = pyodbc.connect(
+#         'DRIVER={ODBC Driver 17 for SQL Server};'
+#         'Server=187.121.151.19;' 
+#         'Database=DB_BASE;'
+#         'UID=user_allnexus;'
+#         'PWD=uKl041xn8HIw0WF;'
+#     )
+#     return conn
