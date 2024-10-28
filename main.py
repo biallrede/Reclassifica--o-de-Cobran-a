68f7a04a77bae9bb7db08684716661c9dda3b418
biallrede <biallrede@gmail.com>
2024-10-28 14:44:23 -0300

    correção dos destinatários e hora de execução.

--- main.py
+++ main.py
@@ -91,17 +91,17 @@ def verifica_servico_forma_cobranca():
         
 
 
-# scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler()
 
-# def rotina1():
-#     verifica_servico_forma_cobranca()
+def rotina1():
+    verifica_servico_forma_cobranca()
     
 
-# schedule.every().day.at("09:23").do(rotina1)
-# scheduler.start()
+schedule.every().day.at("17:55").do(rotina1)
+scheduler.start()
 
-# while (1 == 1):
-#     schedule.run_pending()
-#     threading.Event().wait(1)
+while (1 == 1):
+    schedule.run_pending()
+    threading.Event().wait(1)
 
-verifica_servico_forma_cobranca()
+# verifica_servico_forma_cobranca()
